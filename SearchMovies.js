0bac6b5052fca9ea431f6cac7dbc860c1efe9a02
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-16 22:08:17 +0100

    Added functionality to manage state

---
+++ SearchMovies.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react"
+
+function Movie() {
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+
+
+    const SearchMovies = async (event) => {
+        event.preventDefault();
+        //console.log("submitting")
+
+        const query = "Legally Blonde"
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=f5a8eb46d22c977d682b2a697a3ed561&language=en-US&query=${query}&page=1&include_adult=false`
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            //console.log(data)
+            setMovies(data.results);
+        } catch (err) {
+            console.error(err);
+        }
+
+    }
+
+    return (
+        <div>
+            <form className="form">
+                <label
+                    className="label"
+                    htmlFor="query"
+                    onSubmit={SearchMovies}>Movie Name</label>
+
+                <input
+                    className="input"
+                    type="text"
+                    name="query"
+                    placeholder="i.e. Legally Blonde" 
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}/>
+
+
+                <button className="button" type="submit">Search</button>
+
+            </form>
+        </div>
+    )
+}
+
+export default SearchMovies
