01d4402cfc75a0d722a6600a6ddf452d5add1340
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-16 22:35:01 +0100

    Project finished

--- SearchMovies.js
+++ SearchMovies.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 
+import MovieCard from "./MovieCard"
+
 function Movie() {
     const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
@@ -45,22 +47,7 @@ function Movie() {
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                        />
-
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>RELEASE DATA: {movie.release_data}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-
-                        </div>
-
-
-                    </div>
+                    <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div>
         </div>
